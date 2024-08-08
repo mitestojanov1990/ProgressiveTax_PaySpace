8beb1f0d5d6490338898db59e3036f1d364f0d09
Mite <stojanov1990@gmail.com>
2024-08-08 19:23:17 +0200

    init

---
+++ PaySpace.Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,61 @@
+﻿using MapsterMapper;
+
+using Microsoft.AspNetCore.Mvc;
+
+using PaySpace.Calculator.API.Models;
+using PaySpace.Calculator.Data.Models;
+using PaySpace.Calculator.Services.Abstractions;
+using PaySpace.Calculator.Services.Exceptions;
+using PaySpace.Calculator.Services.Models;
+
+namespace PaySpace.Calculator.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public sealed class CalculatorController(
+        ILogger<CalculatorController> logger,
+        IHistoryService historyService,
+        IPostalCodeService postalCodeService,
+        IMapper mapper)
+        : ControllerBase
+    {
+        [HttpPost("calculate-tax")]
+        public async Task<ActionResult<CalculateResult>> Calculate(CalculateRequest request)
+        {
+            try
+            {
+                var result = 0;
+                var postalCode = request.PostalCode ?? "Unknown";
+                var currentCalculatorType = await postalCodeService.CalculatorTypeAsync(postalCode);
+                if (currentCalculatorType == null)
+                {
+                    throw new CalculatorException();
+                }
+
+                await historyService.AddAsync(new CalculatorHistory
+                {
+                    Tax = 0,
+                    Calculator = currentCalculatorType.Value,
+                    PostalCode = postalCode,
+                    Income = request.Income
+                });
+
+                return this.Ok(mapper.Map<CalculateResultDto>(result));
+            }
+            catch (CalculatorException e)
+            {
+                logger.LogError(e, e.Message);
+
+                return this.BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("history")]
+        public async Task<ActionResult<List<CalculatorHistory>>> History()
+        {
+            var history = await historyService.GetHistoryAsync();
+
+            return this.Ok(mapper.Map<List<CalculatorHistoryDto>>(history));
+        }
+    }
+}
---
+++ PaySpace.Calculator.Web.Services/CalculatorHttpService.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http.Json;
+
+using PaySpace.Calculator.Web.Services.Abstractions;
+using PaySpace.Calculator.Web.Services.Models;
+
+namespace PaySpace.Calculator.Web.Services
+{
+    public class CalculatorHttpService : ICalculatorHttpService
+    {
+        private readonly HttpClient _httpClient;
+        public CalculatorHttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<PostalCode>> GetPostalCodesAsync()
+        {
+            var response = await _httpClient.GetAsync("api/posta1code");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Cannot fetch postal codes, status code: {response.StatusCode}");
+            }
+
+            return await response.Content.ReadFromJsonAsync<List<PostalCode>>() ?? [];
+        }
+
+        public async Task<List<CalculatorHistory>> GetHistoryAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CalculateResult> CalculateTaxAsync(CalculateRequest calculationRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
